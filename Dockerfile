1697d665be0275b8de9417f71fd4f3607e9f0304
iBenzene <ibenzenex@gmail.com>
2025-07-26 17:14:33 +0800

    修改了一些设置，并增加了对于自定义 OS 的配置

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,21 @@
 FROM golang AS builder
+
 WORKDIR /root
 RUN git clone https://github.com/nezhahq/agent
+
 WORKDIR /root/agent/cmd/agent
 RUN env CGO_ENABLED=0 \
     go build -v -trimpath -ldflags \
     "-s -w -X github.com/nezhahq/agent/pkg/monitor.Version=1.13.0"
+
 FROM alpine
+
+# Copy the binary from the builder stage
 RUN apk add --no-cache util-linux
-COPY --from=builder /root/agent/cmd/agent/agent /cgent
-ENV SECRET=default_secret \
-    SERVER=default_server \
-    TLS=false
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-ENTRYPOINT ["/entrypoint.sh"]
+COPY --from=builder /root/agent/cmd/agent/agent /usr/local/bin/nezha-agent
+
+# Copy the entrypoint script
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+RUN chmod +x /usr/local/bin/entrypoint.sh
+
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,36 @@
 #!/bin/sh
-CONFIG_FILE="/root/config.yml"
-if [ ! -f "$CONFIG_FILE" ]; then
-    UUID=$(uuidgen)
-    cat <<EOF > "$CONFIG_FILE"
+
+# Check if SECRET and SERVER are set, if not, exit with an error
+if [ -z "${SECRET}" ] && [ -z "${SERVER}" ]; then
+    echo "Error: SECRET and SERVER environment variables must be set."
+    exit 1
+fi
+
+if [ -z "${TLS}" ]; then
+    TLS=false
+fi
+
+# Modified OS information
+if [ -n "${OS}" ] || [ -n "${OS_VERSION}" ]; then
+    if [ -n "${OS}" ]; then
+        sed -i "s/^ID=.*/ID=${OS}/" /etc/os-release
+    fi
+
+    if [ -n "${OS_VERSION}" ]; then
+        sed -i "s/^VERSION=.*/VERSION=${OS_VERSION}/" /etc/os-release
+    fi
+fi
+
+# Create configuration file
+CONFIG_FILE=/opt/nezha-agent/config.yml
+UUID=$(uuidgen)
+
+cat << EOF > $CONFIG_FILE
 client_secret: ${SECRET}
 server: '${SERVER}'
 tls: ${TLS}
 uuid: ${UUID}
 EOF
-fi
-exec /cgent -c="$CONFIG_FILE"
+
+# Start the Nezha Agent
+exec /usr/local/bin/nezha-agent -c=$CONFIG_FILE
